6a86f137eea357e6cb8d94447df9dfb8e7931e23
tredontho <tredontho@gmail.com>
2013-03-03 23:00:26 -0600

    Problem 2

---
+++ 002/002.hs
@@ -0,0 +1,14 @@
+module Main
+
+where
+
+fib :: [Int]
+fib = 1 : 2 : zipWith (+) fib (tail fib)
+
+evenFibs :: [Int]
+evenFibs = filter (\(p) -> p `mod` 2 == 0) fib
+
+answer :: Int
+answer = sum $ takeWhile (\x -> x <= 4000000) evenFibs
+
+main = print answer
