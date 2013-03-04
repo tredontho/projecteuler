491cd139e667c66158fe183a905bd37e1c8c1637
tredontho <tredontho@gmail.com>
2013-03-03 23:32:51 -0600

    Problem 3

---
+++ 003/003.hs
@@ -0,0 +1,29 @@
+module Main
+
+where
+
+import Control.Monad (guard)
+import Data.List (nub)
+
+-- |Just the divisors of an integer, not including itself or 1 for our purposes
+--divisors :: Int -> [Int]
+divisors :: Integer -> [Integer]
+divisors k = nub $ (fst lists) ++ (snd lists)
+    where 
+        lists = unzip divs
+        divs = 
+            do
+                m <- [2..floor.sqrt $ fromIntegral k]
+                guard $ k `mod` m == 0
+                return (m,k `div` m)
+
+isPrime :: Integer -> Bool
+isPrime 2 = True
+isPrime x = divisors x == []
+
+primeFactors :: Integer -> [Integer]
+primeFactors x = filter isPrime $ divisors x
+
+answer = maximum $ primeFactors 600851475143
+
+main = print answer
